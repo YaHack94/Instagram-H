1740f485c8f09b049a9e4e1fb347656704146a9e
YaHack94 <yahack94@gmail.com>
2021-11-04 13:17:31 +0100

    Mise en œuvre des pages d'erreur 404 et 500.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root to: "users#index"
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  get  '*path', controller: 'application', action: 'render_404'
+  post '*path', controller: 'application', action: 'render_404'
+  get  '*path', controller: 'application', action: 'render_500'
+  post '*path', controller: 'application', action: 'render_500'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
